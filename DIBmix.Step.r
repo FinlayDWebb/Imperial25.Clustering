997ee67b6102f969bc956c3001608e4ae8a82d0e
FinlayDWebb <finlaywebb@rocketmail.com>
2025-07-08 20:10:47 +0100

    nearly there now.

---
+++ DIBmix.Step.r
@@ -0,0 +1,15 @@
+### Here the outline is to do the following:
+# 1. Load the data
+# 2. Cluster the original dataset
+# 3. Cluster the imputed datasets
+# 4. Compare the ARI for each
+
+# This works, because instead of measuring the imputed data against the datasets ground truths (i.e. categories)
+# We instead test it relatively against the clustererd data before imputation, that becomes our ground truth.
+# This means that how well imputation preserves the natural structure of the data, rather than, how well the imputation
+# preserves the supervised learning potential of the data. 
+
+# We want to know strenghts/weaknesses with general datatypes, non-linearity, homogeneity, etc. Not how well imputation
+# preserves the ability to predict a target variable (like income, or age, etc.).
+
+# Load necessary libraries
